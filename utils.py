de5123866424b51979956313a1105006def0d547
Ulorewien <rohanshingre7@gmail.com>
2025-04-21 21:56:10 -0700

    updated utils to load audio model once

--- utils.py
+++ utils.py
@@ -1,10 +1,9 @@
 import whisper
 import yaml
 
-def transcribe_audio(audio_file: str) -> str:
+def transcribe_audio(audio_model: whisper.model.Whisper, audio_file: str) -> str:
     assert audio_file.split(".")[-1] == "wav", "Only .wav files are supported"
     
-    audio_model = whisper.load_model("tiny.en")
     result = audio_model.transcribe(audio_file)
     
     return result["text"]
