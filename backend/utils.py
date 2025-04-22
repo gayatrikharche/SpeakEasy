aa01ac4220f5e4dd91f9022b8554b662bfba5825
Ulorewien <rohanshingre7@gmail.com>
2025-04-22 15:12:04 -0700

    fixed a bug in audio transcription

--- backend/utils.py
+++ backend/utils.py
@@ -15,7 +15,7 @@ def transcribe_audio(audio_model: whisper.model.Whisper, audio_file: str) -> str
     assert audio_file.split(".")[-1] == "wav", "Only .wav files are supported"
     
     result = audio_model.transcribe(audio_file)
-    print(f"Transcribed audio: {result["text"]}")
+    print(f"Transcribed audio: {result['text']}")
     
     return result["text"]
 
